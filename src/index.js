3bf120e076ef2279bb124ab82d7903fef79f5d31
Wexpo Lyu <laosb@lns.unihubs.com>
2016-06-23 06:08:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-export default function wright(fileContent, presets) {
+let info = [];
+
+const fileParsers = {
+  'plain'(fileContent) {
+    return fileContent.split('\n').map((line, arrIndex) => ({
+      line,
+      lineNum: arrIndex + 1,
+    }));
+  },
+};
+
+export default function wright(fileContent, fileType, presets) {
   //TODO: Better mechanism for presets/options.
-  let info = [];
-	
+  if (!fileType) {
+    fileType = 'plain';
+  }
+  const lines = fileParsers[fileType](fileContent);
+  return lint(lines);
 }
