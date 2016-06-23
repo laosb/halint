e4e3648d1103b670b3d28be6fbe6cf3b3b0b5a81
Wexpo Lyu <laosb@lns.unihubs.com>
2016-06-23 12:59:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//TODO: Move fileParsers and fileLinters to separted files.
 const fileParsers = {
   // About parsers:
   // Parsers should just parse anything not in plain text to plain text, with no other markups
@@ -5,6 +6,7 @@ const fileParsers = {
   // need to use origin line numbers for each line in source file, for example, you skipped the
   // code block from line 2 to 6, the line number of the paragraph after the block should still
   // be 7, not 2.
+  //TODO: A better mechanism for loading external parsers as modules.
   'plain'(fileContent, syntaxRules) {
     return {
       lines: fileContent.split('\n').map((line, arrIndex) => ({
@@ -16,7 +18,15 @@ const fileParsers = {
   },
 };
 
-export default function wright(fileContent, fileType, rules) {
+const fileLinters = {
+  'zh_cn'(lines, langRules) {
+    let alerts = [];
+    //TBA
+    return alerts;
+  }
+};
+
+export default function wright(fileContent, fileLang, fileType, rules) {
   //TODO: Better mechanism for rules in preset.
   // `rules` is an object contains all rules in it. Presets are not handled by this function.
   // There is no default rules in this program, so be sure to have it yourself! wright doesn't
@@ -24,12 +34,17 @@ export default function wright(fileContent, fileType, rules) {
   if (!fileType) {
     fileType = 'plain';
   }
+  if (!fileLang) {
+    fileLang = 'zh_cn';
+  }
   
-  const { syntaxRules: syntax, languageRules: language } = rules;
+  const { syntaxRules: syntax, langRules: lang } = rules;
+  
   const { lines, syntaxAlerts } = fileParsers[fileType](fileContent, syntaxRules);
   // Two kinds of alerts there:
   // language alerts: alerts of errors/warnings of (human) language itself by linters.
   // syntax alerts: alerts of errors/warnings of the syntax according to file type by parsers.
   // Both kinds of alerts can have rules defined.
-  let languageAlerts = lint(lines, languageRules);
+  const langAlerts = fileLinters[fileLang](lines, langRules);
+  return { syntax: syntaxAlerts, lang: langAlerts };
 }
