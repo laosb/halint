5ee8eb04767e339051ef7bb01703c41acd6045c8
laosb <laosb@lasteam.net>
2016-06-24 21:46:13 +0800

    Update package.json & test.js.

--- test/test.js
+++ test/test.js
@@ -1,15 +1,38 @@
 var should = require('should'),
     wright = require('../dist/index.js').default;
 
-describe('#plain-zh_cn', function() {
-  it('中西文间空格正确', function() {
-    fileContent = 'Meteor 是一个用于开发现代网页和移动应用的全栈 JavaScript 平台。';
+describe('#zh_cn', function() {
+  it('space-between-han-west 1', function() {
+    fileContent = 'Meteor，一个用于开发现代网页和移动应用的全栈 JavaScript 平台。';
     rules = { syntax: {}, lang: { 'space-between-han-west': true } };
     wright(fileContent, 'zh_cn', 'plain', rules).should.be.empty();
   });
-  it('中西文间空格缺失', function() {
-    fileContent = 'Meteor 是一个用于开发现代网页和移动应用的全栈JavaScript平台。';
+  it('space-between-han-west 2', function() {
+    fileContent = 'Meteor，一个用于开发现代网页和移动应用的全栈JavaScript平台。';
     rules = { syntax: {}, lang: { 'space-between-han-west': true } };
+    wright(fileContent, 'zh_cn', 'plain', rules).should.deepEqual({
+      lang: [ {
+          info: '在 JavaScript 周围缺少空格',
+          lineNum: 1,
+          rule: 'space-between-han-west'
+        } ],
+      syntax: [],
+    });
+  });
+
+  it('avoid-curly-quotes 1', function() {
+    fileContent = '「代码格式检查」是自动检查常规错误或编码风格错误的过程。';
+    rules = { syntax: {}, lang: { 'avoid-curly-quotes': true } };
     wright(fileContent, 'zh_cn', 'plain', rules).should.be.empty();
+  });
+  it('avoid-curly-quotes 2', function() {
+    fileContent = '“代码格式检查”是自动检查常规错误或编码风格错误的过程。';
+    rules = { syntax: {}, lang: { 'avoid-curly-quotes': true } };
+    wright(fileContent, 'zh_cn', 'plain', rules).should.deepEqual({
+      lang: [
+        { info: '避免使用弯引号（“”及‘’）。', lineNum: 1, rule: 'avoid-curly-quotes' }
+      ],
+      syntax: []
+    });
   });
 });
