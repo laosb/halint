2a40ae6af3e321a8f302c60055af19b5cef7ed7e
Wexpo Lyu <laosb@lns.unihubs.com>
2016-07-06 12:40:29 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -43,6 +43,18 @@ describe('#zh_cn', function() {
       syntax: [],
     });
   });
+  it('space-between-han-west 5', function() {
+    fileContent = 'Meteor，一个用于开发现代网页和移动应用的全栈 JavaScript 平台。abc';
+    rules = { syntax: {}, lang: { 'space-between-han-west': true } };
+    wright(fileContent, 'zh_cn', 'plain', rules).should.deepEqual({
+      lang: [ {
+          info: '在 abc 周围缺少空格',
+          lineNum: 1,
+          rule: 'space-between-han-west'
+        } ],
+      syntax: [],
+    });
+  });
 
   it('avoid-curly-quotes 1', function() {
     fileContent = '「代码格式检查」是自动检查常规错误或编码风格错误的过程。';
